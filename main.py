f662ece88c54015fa9e6a358fd60828662851b39
jcox1988 <jcox1988@gmail.com>
2024-01-28 20:37:58 -0600

    Added character_count function

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ def main():
    text = get_text(book_path)
    num_words = word_count(text)
    print(f"{num_words} words found in the document")
+   print(character_count(text))
 
 def word_count(text):
     words = text.split()
     return len(words)
+
+def character_count(text):
+    counts = {}
+    for char in text:
+        if char in counts:
+            counts[char] += 1
+        else:
+            counts[char] = 1
+    return counts
 
 def get_text(path):
     with open(path) as f:
